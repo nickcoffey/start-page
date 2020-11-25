9fa3c71f6b6f213b7d67d97c784a6b7baea648d8
Nick <nicholasjcoffey@gmail.com>
2020-11-25 12:55:51 -0600

    changed clock styling

--- src/components/TopBar/Clock.tsx
+++ src/components/TopBar/Clock.tsx
@@ -1,7 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import styled from 'styled-components'
+import { ThemeContext } from '../../App'
+
+const ColoredText = styled.span<{ color: string }>`
+  color: ${({ color }) => color};
+`
 
 const Clock = () => {
-  const getCurrentTime = () => new Date().toLocaleTimeString()
+  const theme = useContext(ThemeContext)
+
+  const getCurrentTime = () => {
+    const date = new Date().toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' })
+    return { hours: date.slice(0, 2), minutes: date.slice(3, 5), amfm: date.slice(6, 8) }
+  }
 
   const [time, setTime] = useState(getCurrentTime())
 
@@ -11,7 +22,13 @@ const Clock = () => {
     }, 1000)
   })
 
-  return <h1>{time}</h1>
+  return (
+    <h1>
+      {time.hours}
+      <ColoredText color={theme.primary}>:</ColoredText>
+      {time.minutes} {time.amfm}
+    </h1>
+  )
 }
 
 export default Clock
